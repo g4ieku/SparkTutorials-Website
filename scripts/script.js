5ab1b9299703ff43fc659fd54eaf25951f0ea4cb
g4ieku <121449657+g4ieku@users.noreply.github.com>
2025-11-18 14:23:23 +0100

    update css style

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.querySelector(".mobile_menu_toggle i");
   const mobileMenuItems = document.querySelector(".mobile_menu_items");
@@ -5,4 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
   toggleBtn.addEventListener("click", () => {
     mobileMenuItems.classList.toggle("active");
   });
+});
+
+// Change navbar background on scroll
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    document.querySelector("nav").classList.add("navbar_scroll");
+  } else {
+    document.querySelector("nav").classList.remove("navbar_scroll");
+  }
 });
